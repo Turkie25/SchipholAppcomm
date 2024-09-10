77d2943d5f3ada5c2d34494671c61b985e8d873d
Turkie25 <furkan1berat@gmail.com>
2024-09-10 15:16:53 +0200

    users seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,16 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            [
+                'name' => 'Furkan',
+                'email' => 'furkan@gmail.com',
+                'password' => '12345',
+            ],
+            [
+                'name' => 'Samed',
+                'email' => 'samed@gmail.com',
+                'password' => '12345',
+            ],
         ]);
     }
 }
