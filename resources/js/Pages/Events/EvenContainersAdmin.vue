e13564515c239c1317aba0dea83b2914a6ade079
samed <samedbozaslan25@gmail.com>
2024-09-12 09:45:58 +0200

    8

---
+++ resources/js/Pages/Events/EvenContainersAdmin.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex justify-center">
+  <div class="bg-white shadow-md w-9/12	  rounded-lg border border-gray-200 p-4  mt-6">
+    <div class="flex justify-between">
+    <h2 class="text-lg font-semibold mb-2">Name</h2>
+      <button
+          type="submit"
+          class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700"
+      >
+        delete
+      </button>    </div>
+    <div class="space-y-2 flex flex-col gap-6">
+      <div class="flex flex-row gap-5">
+        <label class="block text-gray-700 font-medium text-sm">Location</label>
+        <p class="mt-1 text-gray-900 text-sm"></p>
+
+        <label class="block text-gray-700 font-medium text-sm">Duration</label>
+        <p class="mt-1 text-gray-900 text-sm"></p>
+
+        <label class="block text-gray-700 font-medium text-sm">Capacity</label>
+        <p class="mt-1 text-gray-900 text-sm"></p>
+      </div>
+
+      <div class="flex flex-col gap-2">
+        <div class="flex flex-row gap-5">
+          <label class="block text-gray-700 font-medium text-sm">Start Time</label>
+          <p class="mt-1 text-gray-900 text-sm"></p>
+
+          <label class="block text-gray-700 font-medium text-sm">End Time</label>
+          <p class="mt-1 text-gray-900 text-sm"></p>
+        </div>
+        <div>
+          <label class="block text-gray-700 font-medium text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</label>
+          <p class="mt-1 text-gray-900 text-sm"></p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  </div>
+</template>
+<script setup>
+</script>
---
+++ resources/js/Pages/Events/Events.vue
@@ -0,0 +1,39 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';
+import EventForm from "@/Pages/events/eventForm.vue";
+import EventContainers from "@/Pages/Events/EventContainers.vue";
+import {ref} from "vue";
+import EvenContainersAdmin from "@/Pages/Events/evenContainersAdmin.vue";
+const numberOfContainers = ref(4); // Adjust the number of containers as needed
+
+</script>
+
+<template>
+  <AppLayout title="Dashboard">
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Events
+      </h2>
+    </template>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <event-form>
+
+        </event-form>
+      </div>
+
+      <div class="	flex justify-center  ">
+        <div class="flex flex-wrap gap-3 flex w-10/12">
+
+<!--          <even-containers-admin-->
+<!--              v-for="index in numberOfContainers"-->
+<!--              :key="index"-->
+<!--          ></even-containers-admin>-->
+        </div>
+      </div>
+
+    </div>
+  </AppLayout>
+</template>
